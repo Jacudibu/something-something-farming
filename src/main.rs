c95b26f57dfd45d51a63f74efa55aa4e56703596
jacudibu <jacudibu@gmail.com>
2023-12-31 21:09:44 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+use bevy::window::PresentMode;
+use bevy_screen_diagnostics::{ScreenDiagnosticsPlugin, ScreenEntityDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins
+        .set(ImagePlugin::default_nearest())
+        .set(WindowPlugin {
+            primary_window: Some(Window {
+                present_mode: PresentMode::AutoNoVsync,
+                ..default()
+            }),
+            ..default()
+        }),
+    )
+        .add_plugins(ScreenDiagnosticsPlugin::default())
+        .add_plugins(ScreenFrameDiagnosticsPlugin)
+        .add_plugins(ScreenEntityDiagnosticsPlugin)
+        .run();
+}
