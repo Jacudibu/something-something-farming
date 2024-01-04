b171171191ec0b99e6b1dada795f104aba309a68
jacudibu <jacudibu@gmail.com>
2024-01-04 16:31:22 +0100

    Active tool is now displayed in ui

--- src/game/ui.rs
+++ src/game/ui.rs
@@ -1,9 +1,10 @@
 use crate::prelude::tile_cursor::TileCursor;
-use crate::prelude::WorldData;
+use crate::prelude::{ActiveTool, WorldData};
 use bevy::app::{App, Plugin, Update};
 use bevy::prelude::{error, Query, Res};
-use bevy_egui::egui::Pos2;
+use bevy_egui::egui::{Align2, Pos2};
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use std::ops::Deref;
 
 pub struct UiPlugin;
 impl Plugin for UiPlugin {
@@ -12,7 +13,12 @@ impl Plugin for UiPlugin {
     }
 }
 
-fn ui_system(mut contexts: EguiContexts, cursor: Query<&TileCursor>, world_data: Res<WorldData>) {
+fn ui_system(
+    mut contexts: EguiContexts,
+    cursor: Query<&TileCursor>,
+    world_data: Res<WorldData>,
+    active_tool: Res<ActiveTool>,
+) {
     let cursor = cursor
         .get_single()
         .expect("Multiselection isn't yet supported by debug ui");
@@ -36,5 +42,20 @@ fn ui_system(mut contexts: EguiContexts, cursor: Query<&TileCursor>, world_data:
                 cursor.tile_pos.x, cursor.tile_pos.y
             ));
             ui.label(format!("Tile: {:?}", tile))
+        });
+
+    egui::Window::new("Active Tool View")
+        .title_bar(false)
+        .collapsible(false)
+        .resizable(false)
+        .anchor(Align2::LEFT_BOTTOM, egui::Vec2::new(0.0, 0.0))
+        .fixed_pos(Pos2::new(5.0, 5.0))
+        .show(contexts.ctx_mut(), |ui| match active_tool.deref() {
+            ActiveTool::Hoe => {
+                ui.label("Hoe");
+            }
+            ActiveTool::Pickaxe => {
+                ui.label("Pickaxe");
+            }
         });
 }
