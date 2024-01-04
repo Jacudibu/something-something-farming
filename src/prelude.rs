4fb530e9041c2f6f4f27d87fa88dc608a1c83a35
jacudibu <jacudibu@gmail.com>
2024-01-04 14:19:24 +0100

    Refactoring

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,9 +1,6 @@
-// TODO: make this nested
-pub(crate) use crate::game::tilemap::prelude::*;
-pub(crate) use crate::game::world_data::*;
-pub(crate) use crate::game::{CursorPos, CHUNK_SIZE};
-
 pub(crate) use {
     crate::game::active_tool::ActiveTool,
-    // TODO: More flatting. todo is mostly Mostly for rustfmt.
+    crate::game::tilemap::prelude::*,
+    crate::game::world_data::*,
+    crate::game::{CursorPos, CHUNK_SIZE},
 };
