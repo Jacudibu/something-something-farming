1bbabf71b5f6e3b59274969cd1702ce5557a54a4
jacudibu <jacudibu@gmail.com>
2024-02-09 11:35:05 +0100

    Reworked SimulationTime to use std::time::Duration

--- src/game/simulation_time.rs
+++ src/game/simulation_time.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use bevy::app::{App, Plugin};
 use bevy::prelude::{in_state, First, IntoSystemConfigs, Res, ResMut, Resource, States, Time};
 
@@ -10,13 +12,6 @@ impl Plugin for SimulationTimePlugin {
     }
 }
 
-#[derive(Resource)]
-pub struct SimulationTime {
-    delta_seconds: f32,
-    elapsed_seconds: f32,
-    scale: f32,
-}
-
 #[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
 enum SimulationState {
     #[default]
@@ -25,21 +20,36 @@ enum SimulationState {
 }
 
 fn update(mut simulation: ResMut<SimulationTime>, time: Res<Time>) {
-    simulation.delta_seconds = time.delta_seconds() * simulation.scale;
-    simulation.elapsed_seconds += simulation.delta_seconds;
+    simulation.advance(time.delta());
+}
+
+#[derive(Resource)]
+pub struct SimulationTime {
+    elapsed: Duration,
+    delta: Duration,
+    delta_seconds: f32,
+    scale: f32,
 }
 
 impl Default for SimulationTime {
     fn default() -> Self {
         SimulationTime {
+            delta: Duration::ZERO,
+            elapsed: Duration::ZERO,
             delta_seconds: 0.0,
-            elapsed_seconds: 0.0,
             scale: 1.0,
         }
     }
 }
 
 impl SimulationTime {
+    fn advance(&mut self, delta: Duration) {
+        self.delta = delta.mul_f32(self.scale);
+        self.delta_seconds = delta.as_secs_f32();
+
+        self.elapsed += self.delta;
+    }
+
     #[inline]
     pub fn delta_seconds(&self) -> f32 {
         self.delta_seconds
@@ -51,7 +61,7 @@ impl SimulationTime {
     }
 
     #[inline]
-    pub fn elapsed_seconds(&self) -> f32 {
-        self.elapsed_seconds
+    pub fn elapsed_seconds(&self) -> u64 {
+        self.elapsed.as_secs()
     }
 }
